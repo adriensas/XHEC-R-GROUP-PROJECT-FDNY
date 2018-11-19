82b7d65b8b5ff368f610d55fa2fcd80f4f10d5be
Adrien <adrien.sas@gmail.com>
2018-11-19 12:04:23 +0100

    make empty folder appear

---
+++ devstuffs.R
@@ -0,0 +1,8 @@
+library(devtools)
+library(roxygen2)
+library(usethis)
+library(testthat)
+library(knitr)
+
+Authors@R: c(person("Adrien", "Sasportes", email =
+                      "adrien.sasportes@polytechnique.edu", role = c("aut", "cre")))
