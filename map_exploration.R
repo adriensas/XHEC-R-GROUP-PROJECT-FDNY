79e6e738891e304704605fd30b021af86c9a9156
HenriMatalon <henri.matalon@polytechnique.edu>
2018-11-30 00:41:28 +0100

    Adding a NYC map with every Fire Company

--- map_exploration.R
+++ map_exploration.R
@@ -1,4 +1,13 @@
-install.packages("sf")
-install.packages("raster")
+#install.packages("sf")
+#install.packages("raster")
 library("sf")
 library("raster")
+library("tidyverse")
+
+nyc_map_fc <- st_read(dsn = "data/nyfc",
+                      layer = "nyfc",
+                      quiet = TRUE)
+
+nyc_map_fc <- nyc_map_fc %>% 
+  mutate(FireDiv = factor(FireDiv),
+         FireBN = factor(FireBN))
