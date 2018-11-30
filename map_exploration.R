51156571a6a0e567aa762aa089c5ca656ad2ba48
HenriMatalon <henri.matalon@polytechnique.edu>
2018-11-30 01:33:31 +0100

    Adding a proper database

--- map_exploration.R
+++ map_exploration.R
@@ -1,8 +1,10 @@
 #install.packages("sf")
 #install.packages("raster")
+#install.packages("rgdal")
 library("sf")
 library("raster")
 library("tidyverse")
+library("rgdal")
 
 nyc_map_fc <- st_read(dsn = "data/nyfc",
                       layer = "nyfc",
@@ -11,3 +13,9 @@ nyc_map_fc <- st_read(dsn = "data/nyfc",
 nyc_map_fc <- nyc_map_fc %>% 
   mutate(FireDiv = factor(FireDiv),
          FireBN = factor(FireBN))
+
+nyc_map_firebox <- st_read(dsn = "data/FDNY_Box_Locations.kml")
+
+test <- read_xml("data/FDNY_Box_Locations.kml")
+
+#https://www.google.com/maps/d/u/0/viewer?hl=en&mid=1P57skjWUo0KYfsinVweKLAcnAHA&ll=40.71433439509608%2C-73.88551614404662&z=11
