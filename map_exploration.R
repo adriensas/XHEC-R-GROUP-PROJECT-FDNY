d4e082a69b9c64d26f7ebcf7ed50b46f447e9cf9
HenriMatalon <henri.matalon@polytechnique.edu>
2018-11-30 01:56:34 +0100

    Using XML to solve the layer name issue in the kml file representing the position of the fire boxes

--- map_exploration.R
+++ map_exploration.R
@@ -5,6 +5,7 @@ library("sf")
 library("raster")
 library("tidyverse")
 library("rgdal")
+library("xml2")
 
 nyc_map_fc <- st_read(dsn = "data/nyfc",
                       layer = "nyfc",
@@ -17,5 +18,18 @@ nyc_map_fc <- nyc_map_fc %>%
 nyc_map_firebox <- st_read(dsn = "data/FDNY_Box_Locations.kml")
 
 test <- read_xml("data/FDNY_Box_Locations.kml")
+test <- read_xml("D://henri/Documents/2018 Polytechnique annee 3/Data Science for Business/RProject/XHEC-R-GROUP-PROJECT-FDNY/data/FDNY_Box_Locations.kml")
+
+idx <- 0
+xml_find_all(test, ".//Folder/name") %>%
+  walk(~{
+    idx <<- idx + 1
+    xml_text(.x) <- sprintf("%s-%s", idx, xml_text(.x))
+  })
+
+write_xml(test, "D://henri/Documents/2018 Polytechnique annee 3/Data Science for Business/RProject/XHEC-R-GROUP-PROJECT-FDNY/data/FDNY_Box_Locations_2.kml")
+
+test2 <- read_xml("D://henri/Documents/2018 Polytechnique annee 3/Data Science for Business/RProject/XHEC-R-GROUP-PROJECT-FDNY/data/FDNY_Box_Locations_2.kml")
+
 
 #https://www.google.com/maps/d/u/0/viewer?hl=en&mid=1P57skjWUo0KYfsinVweKLAcnAHA&ll=40.71433439509608%2C-73.88551614404662&z=11
