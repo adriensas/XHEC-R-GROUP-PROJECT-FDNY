085f9b2868ac2e248b36e5f07bc859e07ba8e8b5
Adrien <adrien.sas@gmail.com>
2018-12-03 17:46:12 +0100

    Change of test

--- tests/testthat/test-filter.R
+++ tests/testthat/test-filter.R
@@ -2,17 +2,26 @@ context("test-filter")
 
 library(dplyr)
 
-load("../../data-raw/data.RData")
+data.test <- readRDS("../../data/fdny-data.rds")
 
 input <- list()
-input$ZIP_CODE = "10451"
-input$TIME_INTERVAL = c(1417343844, 1432982244)
-filtered.data <- filter_fdny(data.use, input)
+input$zip_code = c("10451")
+input$time_interval = c(
+    as.POSIXct(1417343844, origin='1970-01-01'),
+    as.POSIXct(1417343844, origin='1970-01-01')
+  )
+filtered.data <- filter_fdny(data.test, input)
 
 test_that("It gives a subset of the full df :", {
-  expect_equal(filtered.data %>% arrange(IM_INCIDENT_KEY) %>% head(dim(data.use)[1]),
-               data.use
-              )
+  filtered.data %>%
+    select(IM_INCIDENT_KEY) %>%
+    unique(IM_INCIDENT_KEY) %>%
+    pull()
+  print(filtered.data)
+  expect_equal(
+    filtered.data %>% arrange(IM_INCIDENT_KEY) %>% head(dim(data.use)[1]),
+    data.test
+  )
 })
 
 test_that("ZIP_CODE is fitered as expected :", {
