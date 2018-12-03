1952497167e1c101e3c17d030d1b6ad9464e528c
Adrien <adrien.sas@gmail.com>
2018-12-03 18:53:45 +0100

    Cleaning files, improved date comparison and computations

--- R/get-deployment-time.R
+++ R/get-deployment-time.R
@@ -15,16 +15,7 @@ get_deployment_time <- function(df) {
   #compute column for deployment time
   #TODO how to return an error if negative in a clean way?
   deployment_time <- df %>%
-    mutate(depart_time =
-             (as.numeric(substr(INCIDENT_DATE_TIME,12,13))*3600) +
-             (as.numeric(substr(INCIDENT_DATE_TIME,15,16))*60) +
-             (as.numeric(substr(INCIDENT_DATE_TIME,18,19))),
-           arrival_time =
-             (as.numeric(substr(ARRIVAL_DATE_TIME,12,13))*3600) +
-             (as.numeric(substr(ARRIVAL_DATE_TIME,15,16))*60) +
-             (as.numeric(substr(ARRIVAL_DATE_TIME,18,19)))) %>%
-    mutate(col = case_when(arrival_time > depart_time ~ arrival_time - depart_time,
-                           arrival_time < depart_time ~ (86400 - arrival_time) + depart_time)) %>%
+    mutate(col = as.numeric(INCIDENT_DATE_TIME) - as.numeric(ARRIVAL_DATE_TIME)) %>%
     select(col)
 
  return(deployment_time)
