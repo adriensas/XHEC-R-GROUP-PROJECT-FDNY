96ce5d1b949d2b7e5d48444dd894f6f3227c26a5
HenriMatalon <henri.matalon@polytechnique.edu>
2018-12-10 11:04:39 +0100

    Writing the new dataset for fixeboxes as a csv file

--- R/tidy_firebox.R
+++ R/tidy_firebox.R
@@ -31,15 +31,20 @@ layer9 = st_read(dsn = "data/FDNY_Box_Locations.kml",
                  layer = "Fire_Boxes_9.csv")
 nyc_map_firebox_full <- rbind(layer1, layer2, layer3, layer4, layer5, layer6, layer7, layer8, layer9)
 
-
+#Tidying the data and adding the same Borough column as in the original dataset
 nyc_map_firebox_full <- nyc_map_firebox_full %>% 
-  mutate(Name = factor(Name),
-         Borough = case_when(
-           substr(Name, 1, 1) == "Q" ~ "5 -Queens",
+  mutate(Borough = case_when(
+           substr(Name, 1, 1) == "Q" ~ "5 - Queens",
            substr(Name, 1, 1) == "M" ~ "1 - Manhattan",
            substr(Name, 1, 1) == "B" ~ "4 - Brooklyn",
            substr(Name, 1, 1) == "X" ~ "2 - Bronx",
            substr(Name, 1, 1) == "R" ~ "3 - Staten Island",
-           TRUE ~ "AAA"))
-         
-plot(nyc_map_firebox_full %>% select(Name))
+           TRUE ~ "AAA"),
+         Name = substr(Name,2,5),
+         Name = factor(Name))
+
+#Ploting the data
+plot(nyc_map_firebox_full %>% select(Borough))
+
+#Saving the new dataset
+write.csv(nyc_map_firebox_full, "data/nyc_firebox.csv")
