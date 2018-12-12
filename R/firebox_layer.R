2b95438a31f7e8ed9de94e5322b15e11a9a9d7c5
HenriMatalon <45174593+HenriMatalon@users.noreply.github.com>
2018-12-12 16:29:06 +0100

    Updated the function

--- R/firebox_layer.R
+++ R/firebox_layer.R
@@ -1,15 +1,20 @@
-nyfc_firebofireboxes <- read.csv("data/firebofireboxes.csv")
+nyfc_firebox <- read.csv("data/firebofireboxes.csv")
 
-plot_firebofireboxes <- function(fireboxes){
+plot_firebox <- function(fireboxes){
   if (str_sub(fireboxes,1,1) %in% c("B", "M", "Q", "R", "fireboxes") && str_length(fireboxes) == 5){
-    nyfc_firebofireboxes %>% 
+    nyfc_firebox %>% 
       filter(Name %in% fireboxes) %>%
       leaflet() %>%
-      addProviderTiles("CartoDB") %>%
-      addCircleMarkers(lng = ~lon, lat = ~lat, label = ~paste0("<b>", Name, "</b>", "<br/>", "num of interventions"), 
-        radius = 2, color = "red")
+      addProviderTiles("CartoDB", group = "CartoDB") %>%
+      addProviderTiles("Esri", group = "Esri") %>%
+      addLayersControl(baseGroups = c("CartoDB", "Esri")) %>% 
+      addCircleMarkers(lng = ~lon, lat = ~lat, label = ~paste0("<b>", htmlEscape(Name), "</b>", "<br/>", "num of interventions"),  radius = 2, color = "red", clusterOptions = markerClusterOptions())
   }
   else {
-    "Please enter a correct firebofireboxes number."
+    "Please enter a correct firebox number."
   }
 }
+#Number of intervention to be changed in : get_inteventions_per_bofireboxes(fireboxes)
+#Intervention duration time : get_intervention_duration(fireboxes)
+#get_deployment_time(fireboxes)
+#get_nb_units(fireboxes)
