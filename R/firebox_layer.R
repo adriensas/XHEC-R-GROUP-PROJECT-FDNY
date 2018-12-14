8064402fc0f3874e5431a3a81a9f3b8710c31df3
Adrien <adrien.sas@gmail.com>
2018-12-14 10:14:24 +0100

    small corrections

--- R/firebox_layer.R
+++ R/firebox_layer.R
@@ -21,33 +21,36 @@ plot_firebox <- function(fireboxes_df){
     nyc_map_firebox_full_filtered <- nyc_map_firebox_full_filtered %>%
       mutate(Name = factor(Name, fct_lvl)) %>%
       left_join(fireboxes_df, by = c("Name"="fire_box"))
+    if (dim(nyc_map_firebox_full_filtered)[1] > 0) {
+      max_n <- max(fireboxes_df$`Number of Intervention`, na.rm = TRUE)
+      min_n <- min(fireboxes_df$`Number of Intervention`, na.rm = TRUE)
 
-    max_n <- max(fireboxes_df$`Number of Intervention`, na.rm = TRUE)
-    min_n <- min(fireboxes_df$`Number of Intervention`, na.rm = TRUE)
+      max_interv_time <- max(fireboxes_df$`Mean Intervention Duration`, na.rm = TRUE)
+      min_interv_time <- min(fireboxes_df$`Mean Intervention Duration`, na.rm = TRUE)
 
-    max_interv_time <- max(fireboxes_df$`Mean Intervention Duration`, na.rm = TRUE)
-    min_interv_time <- min(fireboxes_df$`Mean Intervention Duration`, na.rm = TRUE)
+      pal <- colorQuantile("Reds", nyc_map_firebox_full_filtered$`Mean Intervention Duration`, n = 7)
 
-    pal <- colorQuantile("Reds", nyc_map_firebox_full_filtered$`Mean Intervention Duration`, n = 7)
-
-    nyc_map_firebox_full_filtered %>%
-      mutate(
-        lon = st_coordinates(geometry)[,1],
-        lat = st_coordinates(geometry)[,2],
-        size = 2+70*(`Number of Intervention`-min_n)/max_n
-      ) %>%
-      leaflet() %>%
-      addProviderTiles("CartoDB", group = "CartoDB") %>%
-      addLayersControl(baseGroups = c("CartoDB", "Esri")) %>%
-      addCircleMarkers(
-        opacity = 1,
-        lng = ~lon, lat = ~lat,
-        popup = ~paste0("<b>",Name,"</b>", "<br/>Int.Dur : ", `Mean Intervention Duration`, "<br/>Nb units : ", `Mean Number of Units`, "<br/>Number : ", `Number of Intervention`),
-        radius = ~size,
-        color = ~pal(`Mean Intervention Duration`),
-        clusterOptions = markerClusterOptions()
-      )
+      nyc_map_firebox_full_filtered %>%
+        mutate(
+          lon = st_coordinates(geometry)[,1],
+          lat = st_coordinates(geometry)[,2],
+          size = 2+70*(`Number of Intervention`-min_n)/max_n
+        ) %>%
+        leaflet() %>%
+        addProviderTiles("CartoDB", group = "CartoDB") %>%
+        addLayersControl(baseGroups = c("CartoDB", "Esri")) %>%
+        addCircleMarkers(
+          opacity = 1,
+          lng = ~lon, lat = ~lat,
+          popup = ~paste0("<b>",Name,"</b>", "<br/>Int.Dur : ", `Mean Intervention Duration`, "<br/>Nb units : ", `Mean Number of Units`, "<br/>Number : ", `Number of Intervention`),
+          radius = ~size,
+          color = ~pal(`Mean Intervention Duration`),
+          clusterOptions = markerClusterOptions()
+        )
+    } else {
+      return(leaflet())
+    }
   } else {
-    return("No statistics to display.")
+    return(leaflet())
   }
 }
