fef74ebf6e9db20beeb39ba9ab49b5df0c687785
Adrien <adrien.sas@gmail.com>
2018-12-14 10:24:17 +0100

    Data rmd

--- devstuffs.R
+++ devstuffs.R
@@ -15,13 +15,12 @@ usethis::use_test("get_nb_units")
 usethis::use_test("statistic_fdny")
 
 usethis::use_vignette('cr_reunion_laures_20181123')
+usethis::use_vignette('tidy_firebox_data')
+usethis::use_vignette('tidy_incidents')
 
 devtools::check()
 
 devtools::load_all()
-
-load("../data/incidents.RData")
-devtools::use_data(tidy_incidents, tidy_incidents)
 
 nyfc_zip <- st_read(dsn = "data/ZIP_CODE_040114",
                     layer = "ZIP_CODE_040114",
